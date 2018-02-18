8e312b1e9cad37ef869518caa8df6f85a5cbf60d
shastaclokey1 <shasta.clokey@vmuv.io>
2018-02-17 16:14:39 -0800

    made it so user is forced to turn on bluetooth(action window just keeps popping up if they don't lolz) list view is then updated with the list of paired bluetooth devices

--- MotusApp/app/src/main/java/com/clokey/shasta/motusapp/MainActivity.java
+++ MotusApp/app/src/main/java/com/clokey/shasta/motusapp/MainActivity.java
@@ -15,12 +15,12 @@ public class MainActivity extends AppCompatActivity
 
     //Done make a "pairedDevice" class with parameters(String deviceName, boolean isAvailable, boolean isConnected)
     //ToDo make a bluetooth static class that handles all data protocol activities
-        //ToDo add a function that polls the paired items list and returns a list of "paired devices" objects
+        //Done add a function that polls the paired items list and returns a list of "paired devices" objects
         //ToDo add a function that connects to a to a device on the "paired devices list"
-        //ToDo add a static arrayList of pairedDevices in the bluetooth utils class that holds all the paired devices
+        //Done add a static arrayList of pairedDevices in the bluetooth utils class that holds all the paired devices
         //ToDo add a function that disconnects from the current bluetooth connected device
         //ToDo add a static object that stores the bluetooth device that is currently connected
-        //ToDo add a function that add a function that sends a message to the currently connected bluetooth device
+        //ToDo add a function that sends a message to the currently connected bluetooth device
     //ToDo make a Static class that handles all background work for data transmission
         //ToDo give the class an object of the Thread class(or one of its children)
         //ToDo thread should check if bluetooth device is connected
@@ -39,13 +39,23 @@ public class MainActivity extends AppCompatActivity
 
     private final int REQUEST_ENABLE_BT = 1;
 
+    private PairedDevicesAdapter mPairedDevicesAdapter;
+
+    private BluetoothAdapter mBluetoothAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mPairedDevicesAdapter = new PairedDevicesAdapter(this, new ArrayList<PairedDevice>());
+
+        ListView pairedDeviceList = findViewById(R.id.paired_device_list);
+
+        pairedDeviceList.setAdapter(mPairedDevicesAdapter);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null)
         {
             isBluetoothSupported = false;
@@ -56,28 +66,68 @@ public class MainActivity extends AppCompatActivity
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent,REQUEST_ENABLE_BT);
         }
-
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        ArrayList<PairedDevice> pairedDevicesContainer = new ArrayList<>();
-
-        if (pairedDevices.size() > 0)
+        else
         {
-            // There are paired devices. Get the name and address of each paired device.
-            for (BluetoothDevice device : pairedDevices)
+
+            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+            ArrayList<PairedDevice> pairedDevicesContainer = new ArrayList<>();
+
+            if (pairedDevices.size() > 0)
             {
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-                pairedDevicesContainer.add(new PairedDevice(deviceName,deviceHardwareAddress));
+                // There are paired devices. Get the name and address of each paired device.
+                for (BluetoothDevice device : pairedDevices)
+                {
+                    String deviceName = device.getName();
+                    String deviceHardwareAddress = device.getAddress(); // MAC address
+                    pairedDevicesContainer.add(new PairedDevice(deviceName, deviceHardwareAddress));
+                }
+            }
+
+            mPairedDevicesAdapter.addAll(pairedDevicesContainer);
+        }
+        
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == REQUEST_ENABLE_BT)
+        {
+            // Make sure the request was successful
+            if (resultCode == RESULT_CANCELED)
+            {
+                mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (mBluetoothAdapter == null)
+                {
+                    isBluetoothSupported = false;
+                }
+
+                if (!mBluetoothAdapter.isEnabled())
+                {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent,REQUEST_ENABLE_BT);
+                }
+
+            }
+            else if (resultCode == RESULT_OK)
+            {
+                mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+                ArrayList<PairedDevice> pairedDevicesContainer = new ArrayList<>();
+
+                if (pairedDevices.size() > 0)
+                {
+                    // There are paired devices. Get the name and address of each paired device.
+                    for (BluetoothDevice device : pairedDevices)
+                    {
+                        String deviceName = device.getName();
+                        String deviceHardwareAddress = device.getAddress(); // MAC address
+                        pairedDevicesContainer.add(new PairedDevice(deviceName,deviceHardwareAddress));
+                    }
+                }
+                mPairedDevicesAdapter.addAll(pairedDevicesContainer);
             }
         }
-
-        PairedDevicesAdapter adapter = new PairedDevicesAdapter(MainActivity.this, pairedDevicesContainer);
-
-        ListView pairedDeviceList = findViewById(R.id.paired_device_list);
-
-        pairedDeviceList.setAdapter(adapter);
-
-        
     }
 }
 
